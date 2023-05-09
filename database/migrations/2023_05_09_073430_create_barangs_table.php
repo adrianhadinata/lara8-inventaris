55f28709db6fad16c8a81afe92933504b82dcbbc
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2023-05-09 15:14:25 +0700

    Model & migrate barang

---
+++ database/migrations/2023_05_09_073430_create_barangs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_qr', 100);
+            $table->string('nama_barang');
+            $table->integer('kategori_id');
+            $table->string('merk');
+            $table->integer('satuan_id');
+            $table->string('stok');
+            $table->string('lokasi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
